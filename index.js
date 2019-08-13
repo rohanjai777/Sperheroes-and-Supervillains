c4909973545a09ac5c7342f91b12231fb69e70bd
Rohan Arora <rohanjai777@gmail.com>
2019-08-13 18:02:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const supervillains = require("supervillains");
+var supers = supervillains.random();
+console.log(supers);
+const superheroes = require("superheroes");
+var sup = superheroes.random();
+console.log(sup);
